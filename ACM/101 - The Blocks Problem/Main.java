fa8f37f1c3f32e98f33a30b21d213533717cada2
JianjunHuang <903732218@qq.com>
2017-02-28 22:24:15 +0800

    101 but did't complete

---
+++ ACM/101 - The Blocks Problem/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.List;
+public Main{
+	private List<List<Integer>> list;
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		String com1,com2;
+		int pos1,pos2;
+		while(sc.hasNext()){
+
+		}
+	}
+
+	private void moveOnto(){
+
+	}
+}
