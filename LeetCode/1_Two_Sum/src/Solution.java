31dede0d63579d20cdd6d1b3d88e3f5de0367775
jianjunhuang <jianjun.huang@mobisummer.com>
2018-09-25 18:30:22 +0800

    optimization

--- LeetCode/1_Two_Sum/src/Solution.java
+++ LeetCode/1_Two_Sum/src/Solution.java
@@ -1,11 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                int sum = nums[i] + nums[j];
-                if (sum == target) {
-                    return new int[]{i, j};
-                }
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
             }
         }
         return null;
